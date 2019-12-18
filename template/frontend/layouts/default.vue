a51400ebf3903f34ea423ee8f67ad3caf1ec7893
Tony Lampada <tonylampada@gmail.com>
2019-12-18 08:23:59 -0300

    bugfix - snack alterando store diretamente

--- template/frontend/layouts/default.vue
+++ template/frontend/layouts/default.vue
@@ -37,7 +37,7 @@ export default {
   }),
   computed: {
     snack () {
-      return this.$store.state.snack.snack
+      return JSON.parse(JSON.stringify(this.$store.state.snack.snack))
     }
   },
   methods: {
